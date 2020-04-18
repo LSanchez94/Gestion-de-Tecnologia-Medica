ad8303586dca1c432aabd0a9627bd4b5c422070d
ferlopez98 <62817645+ferlopez98@users.noreply.github.com>
2020-04-18 15:56:19 -0500

    test fer

---
+++ src/views/Departamentos.vue
@@ -0,0 +1,409 @@
+<template>
+  <div class="container-fluid">
+      <div class="row">
+          <div id="PanelNaranja"></div>
+          <div id="CuadroBlanco"></div>
+          <div id="RectanguloAzul"></div>
+          <div id="LogOut"></div>
+          <div id="TextoCalendario"></div>
+          <div id="RectanguloMasOscuro"></div>
+          <div id="TextoDepartamentos"></div>
+          <div id="TextoAltasYBajas"></div>
+          <div id="TextoAgregarProvedor"></div>
+          <div id="TextoConfiguracion"></div>
+          <div id="RectanguloAgregarDpto"></div>
+          <div id="TextoAgregarDpto"></div>
+          <div id="RectanguloInventario"></div>
+          <div id="TextoInventario"></div>
+          <div id="Tabla"></div>
+          <div id="RectanguloDepartamento"></div>
+          <div id="TextoDepartamento"></div>
+          <div id="RectanguloEncargado"></div>
+          <div id="TextoEncargado"></div>
+          <div id="RectanguloContacto"></div>
+          <div id="TextoContacto"></div>
+          <div id="RectanguloRegresar"></div>
+          <div id="TextoRegresar"></div>
+
+                </div>
+</template>
+
+<style scoped>
+
+#RectanguloAzul
+{
+position: absolute;
+width: 1920px;
+height: 161px;
+left: 0px;
+top: -1px;
+}
+
+
+#LogOut
+{
+position: absolute;
+width: 188px;
+height: 75px;
+left: 58px;
+top: 25px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 50px;
+line-height: 75px;
+color: #FFFFFF;
+}
+
+#NombreUsuario
+{
+position: absolute;
+width: 495px;
+height: 75px;
+left: 1336px;
+top: 42px;
+
+
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 50px;
+line-height: 75px;
+color: #FFFFFF;
+
+}
+#PanelNaranja
+{
+position: relative;
+width: 1920px;
+height: 1080px;
+
+background: #FFA41B;
+}
+
+#CuadroBlanco
+{
+position: absolute;
+width: 1412px;
+height: 1080px;
+left: 525px;
+top: 0px;
+
+background: #FFFFFF;
+}
+
+#NombreUsuario
+{
+position: absolute;
+width: 495px;
+height: 75px;
+left: 1336px;
+top: 42px;
+
+
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 50px;
+line-height: 75px;
+color: #FFFFFF;
+
+}
+
+#TextoCalendario
+{
+position: absolute;
+width: 261px;
+height: 68px;
+left: 21px;
+top: 209px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+color: #000000;
+}
+
+#TextoDepartamentos
+{
+position: absolute;
+width: 394px;
+height: 73px;
+left: 21px;
+top: 304px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+
+color: #000000;
+}
+
+#TextoAltasYBajas
+{
+position: absolute;
+width: 273px;
+height: 68px;
+left: 21px;
+top: 404px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+color: #000000;
+}
+
+#TextoAgregarProvedor
+{
+position: absolute;
+width: 446px;
+height: 68px;
+left: 21px;
+top: 499px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+color: #000000;
+}
+
+#TextoConfiguracion
+{
+position: absolute;
+width: 337px;
+height: 68px;
+left: 21px;
+top: 599px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+color: #000000;
+}
+
+#RectanguloMasOscuro
+{
+position: absolute;
+width: 525px;
+height: 99px;
+left: 0px;
+top: 290px;
+
+background: #F09204;
+}
+
+#RectanguloAgregarDpto
+{
+position: absolute;
+width: 573px;
+height: 60px;
+left: 599px;
+top: 230px;
+
+background: #00A8CC;
+border: 1px solid #00A8CC;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+}
+
+#TextoAgregarDpto
+{
+position: absolute;
+width: 476px;
+height: 60px;
+left: 644px;
+top: 230px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 40px;
+line-height: 60px;
+
+color: #FFFFFF;
+}
+
+#RectanguloInventario
+{
+position: absolute;
+width: 393px;
+height: 60px;
+left: 1214px;
+top: 229px;
+
+background: #005082;
+border: 1px solid #005082;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+}
+
+#TextoInventario
+{
+position: absolute;
+width: 209px;
+height: 60px;
+left: 1306px;
+top: 229px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 40px;
+line-height: 60px;
+color: #FFFFFF;
+
+}
+
+
+#Tabla
+{
+position: absolute;
+width: 1230px;
+height: 538px;
+left: 599px;
+top: 392px;
+
+background: #FFFFFF;
+border: 10px solid #00A8CC;
+box-sizing: border-box;
+border-radius: 20px;
+}
+
+#RectanguloDepartamento
+{
+position: absolute;
+width: 410px;
+height: 71px;
+left: 599px;
+top: 356px;
+
+background: #00A8CC;
+border: 1px solid #00A8CC;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+}
+
+#TextoDepartamento
+{
+position: absolute;
+width: 301px;
+height: 60px;
+left: 653px;
+top: 362px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 40px;
+line-height: 60px;
+color: #FFFFFF;
+}
+
+#RectanguloEncargado
+{
+position: absolute;
+width: 410px;
+height: 71px;
+left: 1009px;
+top: 356px;
+
+background: #7ACEE0;
+border: 1px solid #00A8CC;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+}
+
+#TextoEncargado
+{
+position: absolute;
+width: 222px;
+height: 60px;
+left: 1104px;
+top: 362px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 40px;
+line-height: 60px;
+color: #FFFFFF;
+}
+
+#RectanguloContacto
+{
+position: absolute;
+width: 410px;
+height: 71px;
+left: 1419px;
+top: 356px;
+
+background: #1DA0F2;
+border: 1px solid #00A8CC;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+}
+
+#TextoContacto
+{
+position: absolute;
+width: 191px;
+height: 60px;
+left: 1528px;
+top: 362px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 40px;
+line-height: 60px;
+color: #FFFFFF;
+}
+
+#RectanguloRegresar
+{
+position: absolute;
+width: 228px;
+height: 49px;
+left: 1624px;
+top: 988px;
+
+background: #005082;
+border: 1px solid #005082;
+box-sizing: border-box;
+border-radius: 25px;
+}
+
+#TextoRegresar
+{
+position: absolute;
+width: 153px;
+height: 45px;
+left: 1661px;
+top: 992px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 30px;
+line-height: 45px;
+color: #FFFFFF;
+}
+</style>
