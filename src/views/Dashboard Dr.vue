ff666799c5264be8715d9a6d8c8bd80260b685f9
MarianaArr <62817495+MarianaArr@users.noreply.github.com>
2020-04-18 15:36:30 -0500

    Dashboard Dr
    Vista del DashboardDR

---
+++ src/views/Dashboard Dr.vue
@@ -0,0 +1,251 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+          <div id="Rectangle1"></div>
+          <div id="Rectangle2"></div>
+          <div id="LogOut"></div>
+          <div id="DashboardDr" class="col-4">
+          <div id="NombreUsuario"></div>
+          <div id="Contacto"></div>
+          <div id="Configuración"></div>
+          <div id="RectangleRepEq"></div>
+          <div id="REPORTAREQUIPO"></div>
+          <div id="RectangleNSerie"></div>
+          <div id="NSerie"></div>
+          <div id="RectangleDep"></div>
+          <div id="Departamento"></div>
+          <div id="RectangleDescFalla"></div>
+          <div id="DescripcionFalla"></div>
+          <div id="RectangleRep"></div>
+          <div id="REPORTAR"></div>
+          
+
+        </div>
+
+
+
+<style scoped>
+#Rectangle1{ 
+position: absolute;
+width: 1920px;
+height: 161px;
+left: 0px;
+top: -1px;
+background: #030D4A;
+}
+
+#Rectangle2{
+position: absolute;
+width: 1412px;
+height: 1080px;
+left: 508px;
+top: 5px;
+background: #FFFFFF;
+}
+
+#LogOut {
+position: absolute;
+width: 188px;
+height: 75px;
+left: 58px;
+top: 25px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 50px;
+line-height: 75px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#NombreUsuario {
+position: absolute;
+width: 495px;
+height: 75px;
+left: 1336px;
+top: 42px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 50px;
+line-height: 75px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#Contacto {
+position: absolute;
+width: 218px;
+height: 68px;
+left: 21px;
+top: 209px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+/* identical to box height */
+color: #000000;
+}
+
+#Configuración {
+position: absolute;
+width: 337px;
+height: 68px;
+left: 21px;
+top: 294px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 67px;
+/* identical to box height */
+color: #000000;
+}
+
+#RectangleRepEq {
+position: absolute;
+width: 1290px;
+height: 71px;
+left: 541px;
+top: 208px;
+background: #F09204;
+border: 1px solid #F09204;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+}
+
+#REPORTAREQUIPO {
+position: absolute;
+width: 363px;
+height: 60px;
+left: 1032px;
+top: 217px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 40px;
+line-height: 60px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#RectangleNSerie  {
+position: absolute;
+width: 410px;
+height: 48px;
+left: 541px;
+top: 338px;
+background: #7ACEE0;
+border: 1px solid #7ACEE0;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+}
+
+#NSerie {
+position: absolute;
+width: 125px;
+height: 53px;
+left: 685px;
+top: 335px;
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 35px;
+line-height: 52px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#RectangleDep {
+position: absolute;
+width: 846px;
+height: 48px;
+left: 985px;
+top: 338px;
+background: #7ACEE0;
+border: 1px solid #7ACEE0;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+}
+
+#Departamento {
+position: absolute;
+width: 259px;
+height: 53px;
+left: 1278px;
+top: 335px;
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 35px;
+line-height: 52px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#RectangleDescFalla {
+position: absolute;
+width: 1290px;
+height: 262px;
+left: 541px;
+top: 426px;
+background: #7ACEE0;
+border: 1px solid #7ACEE0;
+box-sizing: border-box;
+box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+}
+
+#DescripcionFalla {
+position: absolute;
+width: 452px;
+height: 53px;
+left: 581px;
+top: 444px;
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 35px;
+line-height: 52px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#RectangleRep {
+position: absolute;
+width: 521px;
+height: 49px;
+left: 953px;
+top: 744px;
+background: #005082;
+border: 1px solid #005082;
+box-sizing: border-box;
+border-radius: 25px;
+}
+
+#REPORTAR {
+position: absolute;
+width: 155px;
+height: 45px;
+left: 1137px;
+top: 744px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 30px;
+line-height: 45px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+#DashboardDr {
+  background-color: #ffa41b;
+  height: 100vh;
+  padding-top: 300px;
+}
+</style>
