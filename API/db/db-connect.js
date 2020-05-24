d4f55e0e8577c23d9b36b990256b4907445f7519
CARL0S1398 <62817507+CARL0S1398@users.noreply.github.com>
2020-05-24 12:18:22 -0500

    cambio app reportes dashboard

--- API/db/db-connect.js
+++ API/db/db-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( process.env.MONGODB_URL || 'mongodb://localhost:27017/GDT', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/GDT', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.set('useCreateIndex', true);
 
 module.exports = {mongoose}
